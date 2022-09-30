04ee1acaa458d733b3738e82775ff35092131456
Your Name <you@example.com>
2022-09-30 04:07:34 +0200

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = hatem;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 100;
 
 // Crea una variable booleana:
 const nuevoBool = null;
