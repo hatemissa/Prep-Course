deeb9ccc54a2b45b4595d8f8347233583d3e5187
Your Name <you@example.com>
2022-10-05 20:12:45 +0200

    08-HTML/homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = hatem;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 100;
+const nuevoNum = 300;
 
 // Crea una variable booleana:
 const nuevoBool= true;
